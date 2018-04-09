97185ff63820f4dd4bbf85d79a8806fe20225913
Tim Jones <nutpeg@gmail.com>
2018-04-09 19:33:19 +0100

    feat: Temporary app data

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -25,11 +25,51 @@ const defaultData = {
       commentCount: 0,
     },
   },
+  comments: {
+    "894tuq4ut84ut8v4t8wun89g": {
+      id: "894tuq4ut84ut8v4t8wun89g",
+      parentId: "8xf0y6ziyjabvozdd253nd",
+      timestamp: 1468166872634,
+      body: "Hi there! I am a COMMENT.",
+      author: "thingtwo",
+      voteScore: 6,
+      deleted: false,
+      parentDeleted: false,
+    },
+    "8tu4bsun805n8un48ve89": {
+      id: "8tu4bsun805n8un48ve89",
+      parentId: "8xf0y6ziyjabvozdd253nd",
+      timestamp: 1469479767190,
+      body: "Comments. Are. Cool.",
+      author: "thingone",
+      voteScore: -5,
+      deleted: false,
+      parentDeleted: false,
+    },
+  },
+  categories: [
+    {
+      name: "react",
+      path: "react",
+    },
+    {
+      name: "redux",
+      path: "redux",
+    },
+    {
+      name: "udacity",
+      path: "udacity",
+    },
+  ],
 };
 
 class App extends Component {
   render() {
-    return <div className="App">Readable</div>;
+    return (
+      <div className="App" posts={defaultData.posts}>
+        Readable
+      </div>
+    );
   }
 }
 
