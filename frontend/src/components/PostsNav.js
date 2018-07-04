f4ede65945ac1fa1c95208eb189b56deb3fef74c
Tim Jones <nutpeg@gmail.com>
2018-07-04 23:35:01 +0100

    style: Material-ui makeover

--- frontend/src/components/PostsNav.js
+++ frontend/src/components/PostsNav.js
@@ -1,24 +1,44 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const styles = theme => ({
+  root: {
+    width: '90%',
+    maxWidth: 360,
+  },
+});
+
+const MyLink = props => <Link {...props} />;
 
 class PostsNav extends Component {
   render() {
-    const categories = this.props.categories;
+    const { classes, categories } = this.props;
     return (
       <nav className="nav">
-        <ul>
-          <li>
-            <Link to="/">All</Link>
-          </li>
-          {categories.map(category => (
-            <li key={category.name}>
-              <Link to={`/${category.path}`}>{category.name}</Link>
-            </li>
-          ))}
-        </ul>
+        <div className={classes.root}>
+          <List component="nav">
+            <ListItem button component={MyLink} to="/">
+              <ListItemText primary="All" />
+            </ListItem>
+            {categories.map(category => (
+              <ListItem
+                button
+                key={category.name}
+                component={MyLink}
+                to={`/${category.path}`}
+              >
+                <ListItemText primary={category.name} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </nav>
     );
   }
 }
 
-export default PostsNav;
+export default withStyles(styles)(PostsNav);
