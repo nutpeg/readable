925c330f8af12071006e5ddb706c3d9a5d0f32cd
Tim Jones <nutpeg@gmail.com>
2018-07-05 09:20:04 +0100

    style: Change to lambda funcs

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,19 +1,18 @@
-import { getPosts } from "../utils/api.js";
+import { getPosts } from '../utils/api.js';
 
-export const FETCH_POSTS_SUCCEEDED = "FETCH_POSTS_SUCCEEDED";
-export const FETCH_POSTS_STARTED = "FETCH_POSTS_STARTED";
+export const FETCH_POSTS_SUCCEEDED = 'FETCH_POSTS_SUCCEEDED';
+export const FETCH_POSTS_STARTED = 'FETCH_POSTS_STARTED';
 
-export function fetchPostsStarted() {
-  return {
-    type: FETCH_POSTS_STARTED,
-  };
-}
+export const fetchPostsStarted = () => ({
+  type: FETCH_POSTS_STARTED,
+});
 
-export function fetchPostsSucceeded(posts) {
-  return { type: FETCH_POSTS_SUCCEEDED, posts };
-}
+export const fetchPostsSucceeded = posts => ({
+  type: FETCH_POSTS_SUCCEEDED,
+  posts,
+});
 
-export function fetchPosts() {
+export const fetchPosts = () => {
   return dispatch => {
     dispatch(fetchPostsStarted());
     getPosts().then(posts => {
@@ -22,4 +21,4 @@ export function fetchPosts() {
       }, 20);
     });
   };
-}
+};
