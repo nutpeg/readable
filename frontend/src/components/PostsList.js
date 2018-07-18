acc74f1772154ea97aeb51c0c8c5b30047a353ab
Tim Jones <nutpeg@gmail.com>
2018-07-18 10:29:10 +0100

    refactor: extract SortOrderControls component

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -1,48 +1,17 @@
 import React from 'react';
+import SortOrderControl from './SortOrderControl';
 import PostSummary from './PostSummary';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    marginBottom: 20,
-    paddingLeft: 0,
-  },
-  formControl: {
-    margin: theme.spacing.unit,
-    marginLeft: 0,
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing.unit * 2,
-  },
-  button: {
-    margin: theme.spacing.unit,
-  },
-});
 
 class PostsList extends React.Component {
-
   handleChange = event => {
     this.props.onSortOrderChange(event.target.value);
   };
 
   render() {
-    const {
-      classes,
-      posts,
-      onDeletePost,
-      onEditPost,
-      sortOrder,
-    } = this.props;
+    const { posts, onDeletePost, onEditPost, sortOrder } = this.props;
     return (
       <div className="content">
         <div className="posts-list__header">
@@ -61,19 +30,11 @@ class PostsList extends React.Component {
           </div>
         </div>
 
-        <form className={classes.root} autoComplete="off">
-          <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="sortOrder">Sort by</InputLabel>
-            <Select
-              value={sortOrder}
-              onChange={this.handleChange}
-              inputProps={{ name: 'sortOrder', id: 'sortOrder' }}
-            >
-              <MenuItem value={'timestamp'}>Most recent</MenuItem>
-              <MenuItem value={'voteScore'}>Most votes</MenuItem>
-            </Select>
-          </FormControl>
-        </form>
+        <SortOrderControl
+          sortOrder={sortOrder}
+          onChange={this.handleChange}
+          inputProps={{ name: 'sortOrder', id: 'sortOrder' }}
+        />
 
         <ul>
           {posts.map(post => (
@@ -91,4 +52,4 @@ class PostsList extends React.Component {
   }
 }
 
-export default withStyles(styles)(PostsList);
+export default PostsList;
---
+++ frontend/src/components/SortOrderControl.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginBottom: 20,
+    paddingLeft: 0,
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    marginLeft: 0,
+    minWidth: 120,
+  },
+});
+
+class SortOrderControl extends React.Component {
+  handleChange = event => {
+    this.props.onSortOrderChange(event.target.value);
+  };
+
+  render() {
+    const { classes, sortOrder, onChange } = this.props;
+    return (
+      <form className={classes.root} autoComplete="off">
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="sortOrder">Sort by</InputLabel>
+          <Select
+            value={sortOrder}
+            onChange={onChange}
+            inputProps={{ name: 'sortOrder', id: 'sortOrder' }}
+          >
+            <MenuItem value={'timestamp'}>Most recent</MenuItem>
+            <MenuItem value={'voteScore'}>Most votes</MenuItem>
+          </Select>
+        </FormControl>
+      </form>
+    );
+  }
+}
+
+export default withStyles(styles)(SortOrderControl);
