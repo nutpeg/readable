694f384ac0dc7ec79153c5dd2ecb92756fabd2f5
Tim Jones <nutpeg@gmail.com>
2018-07-22 18:17:24 +0100

    style: add tooltips to controls

--- frontend/src/components/VotingControls.js
+++ frontend/src/components/VotingControls.js
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import Tooltip from '@material-ui/core/Tooltip';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -24,23 +25,27 @@ const VotingControls = props => {
   return (
     <div className="list__voting-controls">
       <div>
-        <Button
-          className={classes.smallIconButton}
-          size="small"
-          onClick={() => onUpVote(id)}
-        >
-          <ExpandLessIcon className={classes.smallIcon} color="action" />
-        </Button>
+        <Tooltip title="+1" placement="left">
+          <Button
+            className={classes.smallIconButton}
+            size="small"
+            onClick={() => onUpVote(id)}
+          >
+            <ExpandLessIcon className={classes.smallIcon} color="action" />
+          </Button>
+        </Tooltip>
       </div>
       <Typography variant="caption">{`${voteScore} VOTES`}</Typography>
       <div>
-        <Button
-          className={classes.smallIconButton}
-          size="small"
-          onClick={() => onDownVote(id)}
-        >
-          <ExpandMoreIcon className={classes.smallIcon} color="action" />
-        </Button>
+        <Tooltip title="-1" placement="right">
+          <Button
+            className={classes.smallIconButton}
+            size="small"
+            onClick={() => onDownVote(id)}
+          >
+            <ExpandMoreIcon className={classes.smallIcon} color="action" />
+          </Button>
+        </Tooltip>
       </div>
     </div>
   );
